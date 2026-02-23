3d34d781efa56122b583ef89bf7ca61a2f32cb7e
Pavan Sai <mpnsai007@gmail.com>
2026-02-23 17:39:35 +0530

    fixed port binding for render

--- app.py
+++ app.py
@@ -214,5 +214,8 @@ def index():
                            score_color=score_color,
                            report_ready=report_ready)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
